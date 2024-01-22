8f9757c220ab50d07627e481cfddfef2cc3db6d5
Lawunn <lawunnkhaing.tgi@gmail.com>
2024-01-22 11:01:19 +0200

    Added Board

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-export default function Square() {
+//Adding Square component
+function Square({value}) {
   return (
-    <div>
-        <button className="square">X</button>
+    <button className="square">{value}</button>
+    );
+}
 
-    </div>
+
+//Board component
+export default function Borad() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
     );
 }
